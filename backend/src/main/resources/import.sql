04c90d83f53a38d628b89718ceb793731da5c2fb
Gustavo Felipe Scheneider <gusfelipes@gmail.com>
2022-10-09 12:48:03 -0300

    feat: auditing database

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -1,3 +1,4 @@
-INSERT INTO TB_CATEGORY (NAME) VALUES ('Books');
-INSERT INTO TB_CATEGORY (NAME) VALUES ('Electronics');
-INSERT INTO TB_CATEGORY (NAME) VALUES ('Computers');
+INSERT INTO tb_category (name, created_at) VALUES ('Books', NOW());
+INSERT INTO tb_category (name, created_at) VALUES ('Electronics', NOW());
+INSERT INTO tb_category (name, created_at) VALUES ('Computers',NOW());
+
